04c100b2db4ae4ca98600a4963585e86ecf9e245
Rahul62066 <2022ugcs107@nitjsr.ac.in>
2026-04-27 18:22:38 +0530

    export platform publisher service from posts module index

--- src/modules/posts/index.ts
+++ src/modules/posts/index.ts
@@ -2,3 +2,7 @@ export { postsRoutes } from "./posts.routes";
 export { postsService } from "./posts.service";
 export { postsController } from "./posts.controller";
 export { postsRepository, PostsRepository } from "./posts.repository";
+export {
+	platformPublisherService,
+	PlatformPublisherService,
+} from "./platformPublisher.service";
